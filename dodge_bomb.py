8fecead203ccd6b0cd576c56b4349cbb162dc27f
tsujiuta <c0b2210244@edu.teu.ac.jp>
2023-06-20 14:19:32 +0900

    練習問題２

--- dodge_bomb.py
+++ dodge_bomb.py
@@ -19,6 +19,7 @@ def main():
     y = random.randint(0, HEIGHT)
     bd_rct = bd_img.get_rect()
     bd_rct.center = x, y 
+    vx, vy = +5, +5
 
     clock = pg.time.Clock()
     tmr = 0
@@ -29,10 +30,11 @@ def main():
 
         screen.blit(bg_img, [0, 0])
         screen.blit(kk_img, [900, 400])
+        bd_rct.move_ip(vx, vy)
         screen.blit(bd_img, bd_rct)
         pg.display.update()
         tmr += 1
-        clock.tick(10)
+        clock.tick(50)
 
 
 if __name__ == "__main__":
